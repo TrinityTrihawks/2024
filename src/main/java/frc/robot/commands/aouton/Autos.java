6d1c2fe576464092cf9081e141f7f8e9c45528a1
xavier2910 <x.distelzweig@gmail.com>
2024-01-27 10:00:27 -0600

    testing restructure: advance test skips next tests <bug>

--- src/main/java/frc/robot/commands/aouton/Autos.java
+++ src/main/java/frc/robot/commands/aouton/Autos.java
@@ -6,23 +6,27 @@ package frc.robot.commands.aouton;
 
 import frc.robot.commands.test.PrintEnc;
 import frc.robot.subsystems.Drive;
+
 import edu.wpi.first.wpilibj2.command.Command;
 
 public final class Autos {
-  // /** Example static factory for an autonomous command. */
-  // public static Command exampleAuto(ExampleSubsystem subsystem) {
-  //   return Commands.sequence(subsystem.exampleMethodCommand());
-  // }
 
-  public static Command driveAMeter(Drive drive) {
-    return new DriveAMeter(drive);
-  }
+    public static Command driveAMeter(Drive drive) {
+        return new DriveAMeter(drive);
+    }
 
-  public static Command printEnc(Drive drive) {
-    return new PrintEnc(drive);
-  }
+    public static Command printEnc(Drive drive) {
+        return new PrintEnc(drive);
+    }
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
-  }
+    public static Command[] allAutos() {
+        return new Command[] {
+                // put all viable autons here for selection by the sendable chooser and to be
+                // run in preflight
+        };
+    }
+
+    private Autos() {
+        throw new UnsupportedOperationException("This is a utility class!");
+    }
 }
--- src/main/java/frc/robot/commands/test/Testing.java
+++ src/main/java/frc/robot/commands/test/Testing.java
@@ -12,38 +12,75 @@ import frc.robot.subsystems.RobotSubsystem;
 public class Testing {
 
     public static Command preFlight(TestArgs args) {
+
         return Commands.sequence(
-            testDriverControls(args),
-            testAutos(args),
-            Commands.print("preflight checklist complete!")
-        );
+                testDriverControls(args),
+                testAutos(args),
+                Commands.print("preflight checklist complete!"),
+                new IdleDrive(args.drive));
     }
-    
+
     private static Command testAutos(TestArgs args) {
 
         return Commands.sequence(
-            // put auton routines to test here
+                alert("No auton routines")
+        // put auton routines to test here via testAll
         );
     }
 
     private static Command testDriverControls(TestArgs args) {
 
-        final TestState testCompletions = new TestState();
-
-        return Commands.sequence(
-            Commands.runOnce(() -> SmartDashboard.putData("Advance Test", Commands.run(() -> testCompletions.testedArcadeDrive = true))),
-            Commands.deadline(Commands.waitUntil(() -> testCompletions.testedArcadeDrive), Teleop.arcadeDrive(args.drive, args.forward, args.twist)),
-
-            Commands.runOnce(() -> SmartDashboard.putData("Advance Test", Commands.run(() -> testCompletions.testedShooter = true))),
-            Commands.deadline(Commands.waitUntil(() -> testCompletions.testedShooter), Teleop.pushToShoot(args.subsys))
-        );
+        return testAll(
+                Teleop.arcadeDrive(args.drive, args.forward, args.twist),
+                Teleop.pushToShoot(args.subsys));
     }
 
-    private static class TestState {
-        
-        private boolean testedArcadeDrive = false;
-        private boolean testedShooter = false;
-        
+    private static Command alert(String msg) {
+        return Commands.runOnce(() -> SmartDashboard.putString("Test alert", msg));
+    }
+
+    private static Command showTestSequence(Command... commands) {
+
+        String[] output = new String[commands.length];
+
+        for (int i = 0; i < commands.length; ++i) {
+            output[i] = commands[i].getClass().getName();
+        }
+
+        return Commands.runOnce(() -> {
+            SmartDashboard.putStringArray("Queued Tests", output);
+        });
+    }
+
+    private static Command test(Command command) {
+        final BooleanWrapper done = new BooleanWrapper();
+
+        return Commands.sequence(
+                Commands.runOnce(() -> {
+                    SmartDashboard.putData("Advance Test",
+                            Commands.runOnce(() -> {
+                                done.bool = true;
+                                
+                            }));
+                }),
+                Commands.deadline(Commands.waitUntil(() -> done.bool), command));
+    }
+
+    private static Command testAll(Command... commands) {
+
+        Command[] output = new Command[commands.length];
+
+        for (int i = 0; i < commands.length; ++i) {
+            output[i] = test(commands[i]);
+        }
+
+        return Commands.sequence(showTestSequence(commands), Commands.sequence(output));
+    }
+
+    private static class BooleanWrapper {
+
+        private boolean bool = false;
+
     }
 
     public static class TestArgs {
