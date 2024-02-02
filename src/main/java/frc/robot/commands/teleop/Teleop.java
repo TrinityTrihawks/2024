83623217b75baa578585ffa3eb342f74d8762a6a
xavier2910 <x.distelzweig@gmail.com>
2024-02-01 21:03:08 -0600

    readded bits

--- src/main/java/frc/robot/commands/teleop/Teleop.java
+++ src/main/java/frc/robot/commands/teleop/Teleop.java
@@ -4,6 +4,7 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import frc.robot.commands.aouton.Shoot;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.RobotSubsystem;
 
@@ -12,7 +13,23 @@ public class Teleop {
     public static Command arcadeDrive(Drive drive, DoubleSupplier forward, DoubleSupplier twist) {
         return new ArcadeDrive(drive, forward, twist);
     }
-    
+
+    /**
+     * porcelain command to run the basic shoot sequence
+     */
+    public static Command pushToShoot(RobotSubsystem subsys) {
+        return new Shoot(subsys);
+    }
+
+    /**
+     * use this in conjunction with Teleop.warmShooter().
+     * it spins up the feeder wheel on start and stops
+     * all wheels on end.
+     */
+    public static Command shoot(RobotSubsystem subsys) {
+        return new StartEndCommand(() -> subsys.shoot(), () -> subsys.stopShooter(), subsys);
+    }
+
     public static Command runIntake(RobotSubsystem subsys) {
         return new StartEndCommand(() -> subsys.intake(), () -> subsys.stopIntake(), subsys);
     }
