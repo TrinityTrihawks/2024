5606402d385ab37327ac4b96ce73dbc84ab65fd3
xavier2910 <x.distelzweig@gmail.com>
2024-03-11 19:41:13 -0500

    tuned up basic apriltag follower

--- src/main/java/frc/robot/commands/aouton/Autos.java
+++ src/main/java/frc/robot/commands/aouton/Autos.java
@@ -102,8 +102,9 @@ public final class Autos {
     public static Command intake(Intake intake) {
         return Commands.startEnd(() -> intake.run(), () -> intake.stop(), intake);
     }
-    public static Command limelight (Intake intake, Drive drive){
-        return new NoteLimelight(drive , intake);
+
+    public static Command followApriltag(Intake intake, Drive drive) {
+        return new AprilLimelight(drive, intake);
     }
 
     private static Command driveOutRightTimed(Drive drive, double time) {
